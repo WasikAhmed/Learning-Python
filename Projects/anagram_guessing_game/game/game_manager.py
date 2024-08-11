421ffc59f76036580742d066325554e1ec871dd5
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-08-12 00:01:00 +0600

    Add user max attempt feature in anagram_guessing_game

--- Projects/anagram_guessing_game/game/game_manager.py
+++ Projects/anagram_guessing_game/game/game_manager.py
@@ -3,9 +3,11 @@ from .player import Player
 
 
 class GameManager:
-    def __init__(self):
+    def __init__(self, max_attempts=3):
         self.word_provider = WordProvider()
         self.player = Player()
+        self.max_attempts = max_attempts
+        self.attempts = 0
 
     def start_game(self):
         word = self.word_provider.get_random_word()
@@ -13,14 +15,21 @@ class GameManager:
 
         print("Welcome to the Anagram Guessing Game!")
         print(f"Here is your anagram: {anagram}")
+        print(f"You have {self.max_attempts} attempts to guess the correct word")
 
-        while True:
+        while self.attempts < self.max_attempts:
             guess = self.player.get_guess()
+            self.attempts += 1
             if self.check_guess(guess, word):
                 print("Congratulations!!! You've guessed the word correctly.")
                 break
             else:
-                print("Incorrect guess! Try again.")
+                remaining_attempts = self.max_attempts - self.attempts
+                if remaining_attempts > 0:
+                    print(f"Incorrect guess! You have {remaining_attempts} attempts left. Try again.")
+                else:
+                    print("Incorrect guess! You have no attempts left.")
+                    print(f"You've failed! The correct word was: {word}")
 
     @staticmethod
     def check_guess(guess, word):
--- Projects/anagram_guessing_game/main.py
+++ Projects/anagram_guessing_game/main.py
@@ -1,5 +1,5 @@
 from game import GameManager
 
 if __name__ == '__main__':
-    game = GameManager()
+    game = GameManager(max_attempts=3)
     game.start_game()
